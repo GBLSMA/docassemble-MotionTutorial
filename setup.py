6fd44ea81fc1e1051109873128503ad382c9105a
System Administrator <qsteenhuis@gmail.com>
2018-03-21 13:42:20 -0400

    Add sample interview file

---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from setuptools import setup, find_packages
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
+
+standard_exclude = ('*.py', '*.pyc', '*~', '.*', '*.bak', '*.swp*')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
+    out = {}
+    stack = [(convert_path(where), '', package)]
+    while stack:
+        where, prefix, package = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                if os.path.isfile(os.path.join(fn, '__init__.py')):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                        stack.append((fn, '', new_package))
+                else:
+                    stack.append((fn, prefix + name + '/', package))
+            else:
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix+name)
+    return out
+
+setup(name='docassemble.MotionTutorial',
+      version='0.2',
+      description=('A tutorial of basic Docassemble features.'),
+      author='Quinten Steenhuis',
+      author_email='qsteenhuis@gbls.org',
+      license='MIT',
+      url='https://gblsma.github.io/docassemble-MotionTutorial/',
+      packages=find_packages(),
+      namespace_packages=['docassemble'],
+      install_requires=[],
+      dependency_links=[],
+      zip_safe=False,
+      package_data=find_package_data(where='docassemble/MotionTutorial/', package='docassemble.MotionTutorial'),
+     )
+
